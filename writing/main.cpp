5843ee4b096e210a74431a1ad09ceec6bd8e410f
Zeyad Amr <68791488+Zeyad-Amr@users.noreply.github.com>
2022-06-20 21:00:40 +0200

    seekg & seekp

--- writing/main.cpp
+++ writing/main.cpp
@@ -3,12 +3,14 @@
 using namespace std;
 
 int main() {
-    char ch;
+
     fstream myfile;
 
     myfile.open("../file.txt", ios::in|ios::out|ios::trunc|ios::binary);
-
     myfile<<"Hello, world. Hello, again.";
+
+    // seekp to set the cursor at a certian char for PUTING
+
     myfile.seekg(12, ios::beg);
     myfile<<'X'<<'X';
     myfile.seekp(3, ios::cur);
@@ -16,7 +18,12 @@ int main() {
     myfile.seekp(-2, ios::end);
     myfile<<'Z';
 
-    //
+    // seekg to set the cursor at a certian char to GETING
+    myfile.seekg(-2, ios::end);
+    char ch;
+    myfile.read(&ch, 1);
+    cout<<ch<<endl;
 
     myfile.close();
+
 }
